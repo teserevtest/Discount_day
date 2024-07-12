1897568f0d43503dd94ce8be27e8e38238070331
Raconocar_w <stranger_forever_rus112@rambler.ru>
2024-07-12 22:27:19 +0300

    fix adapter

--- src/main/java/ru/ykul/service/FileAdapter.java
+++ src/main/java/ru/ykul/service/FileAdapter.java
@@ -1,26 +1,20 @@
 package ru.ykul.service;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileAdapter {
     private String[] linesArray;
 
-    public FileAdapter(String[] lines) {
-        this.linesArray = lines;
+    public FileAdapter(String[] l) {
+        this.linesArray = l;
     }
 
     public String[] getTrueLines() {
         List<String> lines = new ArrayList<>();
         for (String s : linesArray) {
             lines.add(s.replaceAll("#", "|"));
-            System.out.println(s);
         }
-        if (lines.size() == 0) {
-            return lines.toArray(new String[0]);
-        } else {
-            return (String[]) lines.toArray();
-        }
+        return lines.toArray(new String[0]);
     }
 }
