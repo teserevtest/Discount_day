7e08078e8a8ae174a5369a27d335a800f80995fa
Raconocar_w <stranger_forever_rus112@rambler.ru>
2024-08-06 13:06:07 +0300

    fix test v2

--- src/test/java/ru/ykul/service/FileOrderServiceTest.java
+++ src/test/java/ru/ykul/service/FileOrderServiceTest.java
@@ -6,6 +6,10 @@ import ru.ykul.model.OrderReport;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -15,6 +19,20 @@ class FileOrderServiceTest {
 
     private final static String ORDER_PATH = Main.class.getClassLoader().getResource("orders").getPath().toString();
     private final FileOrderService fileOrderService = new FileOrderService();
+
+    @AfterEach
+    void clean_test_output() {
+        String fileName = "test_output.txt";
+        try {
+            Files.deleteIfExists(
+                    Paths.get(ORDER_PATH.substring(1) + "/" + fileName));
+        } catch (NoSuchFileException e) {
+            System.out.println(
+                    "No such file/directory exists");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     @DisplayName("0. read txt")
@@ -33,7 +51,6 @@ class FileOrderServiceTest {
         fileText[0] = "2021-02-09T16:00:22#Industrial#8800";
         Assertions.assertEquals(fileText[0], fileOrderService.read(fileName)[0]);
     }
-
 
     @Test
     @DisplayName("2. read empty")
@@ -63,7 +80,7 @@ class FileOrderServiceTest {
     @Test
     @DisplayName("5. write file")
     void fileOrderService_write_shouldWriteOrderToFile() {
-        String fileName = "fileOrderService_write_shouldWriteOrderToFile.txt";
+        String fileName = "test_output.txt";
         String key = "Industrial";
         Double value = 79200.0;
         OrderReport orderReport = new OrderReport();
@@ -95,31 +112,18 @@ class FileOrderServiceTest {
         Double value = 79200.0;
         OrderReport orderReport = new OrderReport();
         orderReport.putToMap(key, value);
-        assertThrows(RuntimeException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             fileOrderService.write(orderReport, fileName);
         }, "Output file name is null!");
     }
 
     @Test
-    @DisplayName("8. write empty report")
-    void fileOrderService_write_shouldThrowException_ifOrderReportIsEmpty() {
-        String fileName = "fileOrderService_write_shouldThrowException_ifOrderReportIsEmpty.txt";
-        OrderReport orderReport = new OrderReport();
-        assertThrows(IllegalArgumentException.class, () -> {
-            fileOrderService.write(orderReport, fileName);
-        }, "OrderReport is empty!");
-    }
-
-
-    @Test
-    @DisplayName("9. write empty file name")
+    @DisplayName("8. write empty file name")
     void fileOrderService_write_shouldThrowException_ifFileNameIsEmpty() {
         String fileName = "";
         OrderReport orderReport = new OrderReport();
-        assertThrows(RuntimeException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             fileOrderService.write(orderReport, fileName);
         }, "Output file name is empty!");
     }
-
-
 }
