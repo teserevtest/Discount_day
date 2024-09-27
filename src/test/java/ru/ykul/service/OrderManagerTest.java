09dbaeb75feb9277cad40888431d78ae160caa88
Raconocar_w <stranger_forever_rus112@rambler.ru>
2024-09-27 14:10:11 +0300

    clear

--- src/test/java/ru/ykul/service/OrderManagerTest.java
+++ src/test/java/ru/ykul/service/OrderManagerTest.java
@@ -50,59 +50,16 @@ public class OrderManagerTest {
         OrderReport orderReport = new OrderReport();
         orderReport.putToMap("Industrial", 79200.0);
 
-//        when(orderParserFactoryMock.getOrderParser(testInFileNamePipe)).thenReturn(new OrderParserByPipe());
-        when(orderParserFactoryMock.getOrderParser(testInFileNameOct)).thenReturn(new OrderParserByOctothorpe());
-        when(fileOrderServiceMock.read(testInFileNamePipe)).thenReturn(fileorderserviceReadRefund);
-        when(orderParserMock.getOrderList(fileorderserviceReadRefund)).thenReturn(orderParserRefund);
+
         when(orderServiceMock.createOrderReport(orderParserRefund, discount, cost, discountStep, bagWeight)).thenReturn(orderReport);
+        when(orderParserMock.getOrderList(fileorderserviceReadRefund)).thenReturn(orderParserRefund);
+        when(fileOrderServiceMock.read(testInFileNamePipe)).thenReturn(fileorderserviceReadRefund);
+        when(orderParserFactoryMock.getOrderParser(testInFileNameOct)).thenReturn(new OrderParserByOctothorpe());
 
 
-//                doReturn(new String[]{"", ""}).when(fileOrderServiceMock).read(testInFileName)]
-
-
-//        var qqqq = orderManager.writeOrderReport();
-
-
-//        OrderParser orderParser = orderParserFactory.getOrderParser(testInFileName);
-//        String[] stringOrders = fileOrderService.read(testInFileName);
-//        List<Order> orders = orderParser.getOrderList(stringOrders);
-//        OrderReport orderReport = orderService.createOrderReport(orders, discount, cost, discountStep, bagWeight);
-//        fileOrderService.write(orderReport, testOutFileName);
         orderManagerMocked.writeOrderReport(testInFileNamePipe, testOutFileName, discount, cost, discountStep, bagWeight);
         verify(orderManagerMocked, atLeastOnce()).writeOrderReport(testInFileNamePipe, testOutFileName, discount, cost, discountStep, bagWeight);
         verify(orderParserMock, atLeastOnce()).getOrderList(fileorderserviceReadRefund);
-
-//        // определяем поведение (результаты)
-//        when(mcalc.subtract(15.0, 25.0)).thenReturn(10.0);
-//        when(mcalc.subtract(35.0, 25.0)).thenReturn(-10.0);
-//
-//        // вызов метода subtract и проверка результата
-//        assertEquals (calc.subtract(15.0, 25.0), 10, 0);
-//        assertEquals (calc.subtract(15.0, 25.0), 10, 0);
-//
-//        assertEquals (calc.subtract(35.0, 25.0),-10, 0);
-//
-//        // проверка вызова методов
-//        verify(mcalc, atLeastOnce()).subtract(35.0, 25.0);
-//        verify(mcalc, atLeast   (2)).subtract(15.0, 25.0);
-//
-//        // проверка - был ли метод вызван 2 раза?
-//        verify(mcalc, times(2)).subtract(15.0, 25.0);
-//        // проверка - метод не был вызван ни разу
-//        verify(mcalc, never()).divide(10.0,20.0);
-
-        /* Если снять комментарий со следующей проверки, то
-         * ожидается exception, поскольку метод "subtract"
-         * с параметрами (35.0, 25.0) был вызван 1 раз
-         */
-        // verify(mcalc, atLeast (2)).subtract(35.0, 25.0);
-
-        /* Если снять комментарий со следующей проверки, то
-         * ожидается exception, поскольку метод "subtract"
-         * с параметрами (15.0, 25.0) был вызван 2 раза, а
-         * ожидался всего один вызов
-         */
-        // verify(mcalc, atMost (1)).subtract(15.0, 25.0);
 
 
     }
